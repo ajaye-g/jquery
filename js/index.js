703718da921b98d9e328751e90e9a19eec08965b
ajaye-g <ajaye.g@techversantinfo.com>
2022-09-15 15:22:06 +0530

    last commit

--- js/index.js
+++ js/index.js
@@ -5,35 +5,54 @@ $('#form').submit(function(e){
 	var lastname = $('#lastname').val();
 	var password = $('#password').val();
 	var email = $('#email').val();
-	var gender = $('#gender');
-	
+	var phno = $('#phno').val();
+	var male = $('#male').val();
+	var female = $('#female').val();
+	var other = $('#other').val();
+	var language1 =$('#language1').val();
+	var language2 =$('#language2').val();
+	var language3 =$('#language3').val();
+	var language4 =$('#language4').val();
 
 
 	$(".error").remove();
 		
 		if(firstname.length<1){
-		$('#firstname').after('<ul><span class="error">name is required</span></ul>');
+			$('#firstname').after('<div class="error">name is required</div>');
 		}
 		
 		if(lastname.length<1){
-		$('#lastname').after('<ul><span class="error">name is required</span></ul>');
+			$('#lastname').after('<div class="error">name is required</div>');
 		}
 
 		if(password.length<5){
-		$('#password').after('<ul><span class="error">password should have 8 character</span></ul>');
+			$('#password').after('<div class="error">password should have 8 character</div>');
 		}
 		if (email.length < 1) {
-     		 $('#email').after('<ul><span class="error">enter the email</span></ul>');
+     			$('#email').after('<div class="error">enter the email</div>');
     		} else {
-     		 var regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      		var validEmail = regEx.test(email);
+     		 	var regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      			var validEmail = regEx.test(email);
       		if (!validEmail) {
-       		 $('#email').after('<ul><span class="error">Enter a valid email</span></ul>');
+       			$('#email').after('<div class="error">Enter a valid email</div>');
       		}
     		}
 		
-
-
+		if(isNaN(phno) || phno.length <10 || phno.length>10){
+			$('#phno').after('<div class="error">enter the phone number</div>');
+		}
+		
+		if($('input[name="sex"]:checked').length == 0){
+		
+			 $('.sx').after('<div class="error">select any</div>');
+		}else{
+		     
+		     }
+		if($('input[name="language"]:checked').length == 0){
+			 $('.checkBox').after('<div class="error">select any</div>');
+		}else{
+		     
+		     }	
   });
 });
 
